1732fc33f27da0cbc50c4deb3f6f224ffc05a5e3
dapousman <daffausman1@gmail.com>
2021-11-02 15:51:08 +0700

    Flutter_chapter3

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import './input.dart';
+import './convert.dart';
+import './result.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double _inputCelcius = 0;
+  double _kelvinResult = 0;
+  double _reamurResult = 0;
+
+  final _celciusNumber = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Konverter Suhu"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Input(_celciusNumber),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Result('Kelvin', _kelvinResult),
+                  Result('Reamur', _reamurResult),
+                ],
+              ),
+              Convert(convert),
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+
+  void convert() {
+    setState(() {
+      _kelvinResult = double.parse(_celciusNumber.text) + 273;
+      _reamurResult = (4 / 5) * double.parse(_celciusNumber.text);
+    });
+  }
+}
